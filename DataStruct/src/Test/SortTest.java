7c807dbe30540b1261fb76e37ea3f7858afa05ce
pinedx <xurenzealiyun.com>
2020-12-07 13:03:02 +0800

    添加插入排序

--- DataStruct/src/Test/SortTest.java
+++ DataStruct/src/Test/SortTest.java
@@ -1,8 +1,15 @@
 package Test;
 
 import algorithm.sort.BubbleSort;
+import algorithm.sort.InsertionSort;
 import algorithm.sort.SelectSort;
 import algorithm.tool.SortTool;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.logging.SimpleFormatter;
 
 /**
  * <功能简述>
@@ -15,16 +22,18 @@ public class SortTest {
     public static void main(String[] args) {
 
 
-            SortTool sortTool = new SortTool(20);
+            SortTool sortTool = new SortTool(10, 0 ,100);
             int[] arr = sortTool.getArrayInt();
             System.out.print("排序前:");
             sortTool.ScanArr(arr);
-
             // 冒泡排序
 //            BubbleSort sort = new BubbleSort();
             // 选择排序
-            SelectSort sort = new SelectSort();
+//            SelectSort sort = new SelectSort();
+            // 插入排序
+            InsertionSort sort = new InsertionSort();
             arr = sort.sort(arr);
+
             System.out.print("排序后:");
             sortTool.ScanArr(arr);
     }
--- DataStruct/src/algorithm/tool/SortTool.java
+++ DataStruct/src/algorithm/tool/SortTool.java
@@ -12,18 +12,30 @@ import java.lang.reflect.Array;
 public class SortTool {
 
     private int custome_size;   // 用户指定大小
+    private int rangeL;
+    private int rangeR;
 
     public SortTool() {
         this.custome_size = 10;
+        this.rangeL = 0;
+        this.rangeR = 1000;
     }
     public SortTool(int size){
         this.custome_size = size;
+        this.rangeL = 0;
+        this.rangeR = 1000;
+    }
+    public SortTool(int size, int l ,int r) {
+        this.custome_size = size;
+        r = l < r ? r : (l + r);
+        this.rangeL = l;
+        this.rangeR = r;
     }
 
     public int[] getArrayInt(){
         int[] arr = new int[custome_size];
         for (int i = 0; i < custome_size; i++) {
-            arr[i] = (int)(Math.random()*100);
+            arr[i] = (int)(Math.random() * (this.rangeR - this.rangeL + 1));
         }
         return arr;
     }
