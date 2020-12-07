924fb8d7c87c57fcec25f49ec9a685d0d1005ff4
pinedx <xurenzealiyun.com>
2020-12-07 22:03:37 +0800

    二叉树的三种递归遍历

---
+++ DataStruct/src/Test/datastruct/BinaryTreeTest.java
@@ -0,0 +1,19 @@
+package Test.datastruct;
+
+import algorithm.datastruct.tree.binary_tree.BiTree;
+
+/**
+ * Title:
+ * Desc:
+ *
+ * @Author: Songlin
+ * @create: 2020/12/7-21:51
+ */
+public class BinaryTreeTest {
+    public static void main(String[] args) {
+        BiTree tree = new BiTree("ABDECFG");
+//        tree.preRootTraverse(tree.getRoot());
+//        tree.inRootTraverse(tree.getRoot());
+        tree.postRootTraverse(tree.getRoot());
+    }
+}
