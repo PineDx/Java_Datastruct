4bdaf749af0ba1c4ba00607e15287cd2edf167a5
pinedx <xurenzealiyun.com>
2020-12-14 20:07:29 +0800

    索引最大堆纠错

--- DataStruct/src/Test/datastruct/tree/MaxHeapTest.java
+++ DataStruct/src/Test/datastruct/tree/MaxHeapTest.java
@@ -16,25 +16,28 @@ public class MaxHeapTest {
         MaxHeap heap = new MaxHeap(10);
         SortTool sortTool = new SortTool(10, 0 ,100);
         int[] arr = sortTool.getArrayInt();
-        System.out.println("原数组:");
+        System.out.print("原数组:\t");
         sortTool.ScanArr(arr);
         for (int value : arr) {
             heap.insert(value);
         }
-        System.out.println("最大堆:");
+        System.out.print("最大堆:\t");
         heap.ScanHeap();
-        System.out.println("\n");
+        System.out.print("\n最大堆排序:\t");
         while (!heap.isEmpty()) {
             System.out.print(heap.extractMax() + " ");
         }
 
+        // 索引最大堆实现
         IndexMaxHeap indexheap = new IndexMaxHeap(10);
 
         for (int i = 0; i < arr.length ; i++ ) {
             indexheap.insert(i, arr[i]);
         }
 
-        System.out.println("索引最大堆的实现:");
+        System.out.print("\n索引最大堆的实现:\t");
+        indexheap.ScanHeap();
+        System.out.print("\n排好序的索引最大堆的实现:\t");
         while (!indexheap.isEmpty()) {
             System.out.print(indexheap.extractMax() + " ");
         }
--- DataStruct/src/algorithm/datastruct/heap/IndexMaxHeap.java
+++ DataStruct/src/algorithm/datastruct/heap/IndexMaxHeap.java
@@ -19,6 +19,10 @@ public class IndexMaxHeap {
     private int capacity;
     private int count;
 
+    /**
+     * 初始化堆
+     * @param capacity 容量大小
+     */
     public IndexMaxHeap(int capacity){
         if (capacity > 0) {
             arr = new int[capacity];
@@ -28,6 +32,11 @@ public class IndexMaxHeap {
         }
     }
 
+    /**
+     * 向堆中插入元素
+     * @param index 索引数组下标
+     * @param k 元素
+     */
     public void insert(int index, int k) {
         assert count + 1 <= capacity : "容量超过最大值";
         assert index >= 0 && index + 1 <= capacity : "索引值异常";
@@ -35,11 +44,16 @@ public class IndexMaxHeap {
         // 堆的数据项
         arr[index] = k;
         // 堆的索引项
-        indexArr[count++] = index;
+        indexArr[index] = index;
         shiftUp(count);
+        count++;
 
     }
 
+    /**
+     * shiftup 的过程
+     * @param k
+     */
     private void shiftUp(int k) {
         int temp;
         while ( k >= 0 && arr[indexArr[k/2]] < arr[indexArr[k]]) {
@@ -50,7 +64,27 @@ public class IndexMaxHeap {
         }
     }
 
+    /**
+     * 获取最大堆的顶上元素
+     * @return
+     */
     public int extractMax() {
+
+        int ret = arr[indexArr[0]];
+        int temp = indexArr[0];
+        indexArr[0] = indexArr[count-1];
+        indexArr[count-1] = temp;
+        count--;
+
+        shiftDwon(0);
+        return ret;
+    }
+
+    /**
+     * 获取最大堆的顶上元素索引 可直接通过get找到该元素
+     * @return
+     */
+    public int extractMaxIndex() {
         assert count  > 0;
         int ret = arr[indexArr[0]];
         int temp = indexArr[0];
@@ -62,11 +96,15 @@ public class IndexMaxHeap {
         return ret;
     }
 
+    /**
+     * shiftdown 的过程
+     * @param k 代表元素在索引中的下标
+     */
     private void shiftDwon(int k) {
 
-        while ( 2*k <= count ) {
+        while ( 2*k <= count-1 ) {
             int j = 2*k + 1;
-            if ( j + 1 <= count && arr[indexArr[j + 1]] > arr[indexArr[j]]) {
+            if ( j + 1 <= count-1 && arr[indexArr[j + 1]] > arr[indexArr[j]]) {
                 j += 1;
             }
             if (arr[indexArr[k]] > arr[indexArr[j]]) break;
@@ -76,18 +114,47 @@ public class IndexMaxHeap {
             k = j;
         }
     }
+
+    /**
+     * 堆的最大元素
+     * @return
+     */
     public int size() {
         return count;
     }
 
+    /**
+     * 堆是否为空
+     * @return
+     */
     public boolean isEmpty() {
         return count == 0;
     }
 
+    /**
+     * 打印堆中元素
+     */
+    public void ScanHeap() {
+        for (int i :
+                indexArr) {
+            System.out.print(arr[i]  + " ");
+        }
+    }
+
+    /**
+     * 通过下标直接获取元素
+     * @param i
+     * @return
+     */
     public int getItem( int i ) {
         return arr[i];
     }
 
+    /**
+     * 改变该元素的值 重新构建最大堆性质
+     * @param i
+     * @param item
+     */
     public void change( int i , int item) {
         arr[i] = item;
 
